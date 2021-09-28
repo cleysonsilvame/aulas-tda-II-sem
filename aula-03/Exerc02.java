6a675f7fbd4b4380da8e0aba5faf32826252b6fb
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-09-28 03:02:06 -0300

    exercício 02 aula 03

---
+++ aula-03/Exerc02.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc02 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    scanner.useLocale(Locale.US);
+
+    float ladoA, ladoB;
+
+    System.out.println("Digite o lado 'A' do triângulo: ");
+    ladoA = scanner.nextFloat();
+
+    System.out.println("Digite o lado 'B' do triângulo: ");
+    ladoB = scanner.nextFloat();
+
+    System.out.println("\n\nA área do triângulo é: " + calculaArea(ladoA, ladoB));
+
+    scanner.close();
+  }
+
+  static float calculaArea(float ladoA, float ladoB) {
+    return ladoA * ladoB;
+  }
+}
